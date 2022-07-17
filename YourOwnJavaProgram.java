46c5bc92d939fb731cf81256650515b576f62be9
khalid <khleadini@gmail.com>
2022-07-17 19:16:04 +0300

    Your Own Java Program

---
+++ YourOwnJavaProgram.java
@@ -0,0 +1,8 @@
+public class YourOwnJavaProgram{
+    public static void main(String[]args){
+        System.out.println("My name is Khaled");
+        System.out.println("I am 25 years old");
+        System.out.println("My hometown is jeddah, SA");
+    }
+
+}
